6b92073b6ab1e0f98f15f8b0043d869c4dda38f9
Kovak <kovac1066@gmail.com>
2014-04-20 05:10:04 -0600

    added headers for chipmunk 6.2, working on steering behavior

--- kivent/kivent/steering.pyx
+++ kivent/kivent/steering.pyx
@@ -1,8 +1,66 @@
 
-from cymunk cimport (GearJoint, PivotJoint, Vec2d, cpVect, cpv,
-    cpFloat, cpBool, cpvunrotate, cpvrotate, cpvdot, cpvsub, cpvnear)
-
+from cymunk cimport (GearJoint, PivotJoint, Vec2d, cpVect, cpv, cpvlength,
+    cpFloat, cpBool, cpvunrotate, cpvrotate, cpvdot, cpvsub, cpvnear,
+    cpvneg, cpvadd, cpvmult, cpvcross, cpvnormalize, cpvdist,
+    cpvdistsq)
 from libc.math cimport atan2
+
+
+
+from random import randint
+
+
+cdef class SteeringAIComponent:
+    cdef float _avoidance_max
+    cdef float _attack_angle
+    cdef float _decision_time
+    cdef float _change_time
+    cdef str _state
+    cdef tuple _target
+
+    def __cinit__(self, float avoidance_max, float change_time, str state):
+        self._avoidance_max = avoidance_max
+        self._attack_angle = 0.0
+        self._change_time = change_time
+        self._decision_time = change_time
+        self._state = state
+        self._target = (None, None)
+
+    property avoidance_max:
+        def __get__(self):
+            return self._avoidance_max
+        def __set__(self, float value):
+            self._avoidance_max = value
+
+    property decision_time:
+        def __get__(self):
+            return self._decision_time
+        def __set__(self, float value):
+            self._decision_time = value
+
+    property attack_angle:
+        def __get__(self):
+            return self._attack_angle
+        def __set__(self, float value):
+            self._attack_angle = value
+
+    property change_time:
+        def __get__(self):
+            return self._change_time
+        def __set__(self, float value):
+            self._change_time = value
+
+    property state:
+        def __get__(self):
+            return self._state
+        def __set__(self, str value):
+            self._state = value
+
+    property target:
+        def __get__(self):
+            return self._target
+        def __set__(self, tuple target):
+            self._target = target
 
 
 cdef class SteeringComponent: 
@@ -76,6 +134,106 @@ cdef class SteeringComponent:
         def __set__(self, float force):
             self._pivot.max_force = force
 
+
+class SteeringAISystem(GameSystem):
+    physics_system = StringProperty(None)
+    steering_system = StringProperty(None)
+    updateable = BooleanProperty(True)
+    view_system = StringProperty(None)
+
+    def generate_component(self, dict args):
+        avoidance_max = args['avoidance_max']
+        change_time = args['change_time']
+        state = args['state']
+        new_component = SteeringAIComponent.__new__(SteeringAIComponent, 
+            avoidance_max, change_time, state)
+        return new_component
+
+    def update(self, dt):
+        cdef list entity_ids = self.entity_ids
+        cdef int entity_id
+        cdef object gameworld = self.gameworld
+        cdef list entities = gameworld.entities
+        cdef SteeringComponent steering_data
+        cdef SteeringAIComponent ai_data
+        cdef PhysicsComponent physics_data
+        cdef PhysicsComponent obs_physics_data
+        cdef str system_id = self.system_id
+        cdef str physics_id = self.physics_system
+        cdef str steering_id = self.steering_system
+        cdef str state
+        cdef float speed
+        cdef Body body
+        cdef str view_id = self.view_system
+        cdef set in_view
+        cdef cpVect target
+        cdef cpVect pos
+        cdef cpVect ob_pos
+        cdef cpVect ob_vel
+        cdef cpVect b_vel
+        cdef cpVect ob_vel_t
+        cdef cpVect avoid_vec
+        cdef cpVect avoid_norm
+        cdef float avoid_max
+        cdef cpVect new_targ
+        cdef cpVect new_av
+        cdef Body ob_body
+        cdef tuple tup_targ
+        execute_decision = self.execute_decision
+        for entity_id in entity_ids:
+            entity = entities[entity_id]
+            steering_data = getattr(entity, steering_id)
+            physics_data = getattr(entity, physics_id)
+            body = physics_data._body
+            ai_data = getattr(entity, system_id)
+            ai_data._decision_time += dt
+            view_data = getattr(entity, view_id)
+            in_view = view_data.in_view
+            speed = steering_data._speed
+            avoid_max = ai_data._avoidance_max
+            state = ai_data._state
+            if ai_data._decision_time >= ai_data._change_time:
+                #execute_decision(steering_data, ai_data, state)
+                ai_data._decision_time = 0.0
+            tup_targ = ai_data._target
+            avoid_vec = cpv(0., 0.)
+            if len(in_view) > 0:
+                for obstacle_id in in_view:
+                    print obstacle_id
+                    ob_ent = entities[obstacle_id]
+                    obs_physics_data = getattr(ob_ent, physics_id)
+                    ob_body = obs_physics_data._body
+                    ob_pos = ob_body._body.p
+                    ob_vel = ob_body._body.v
+                    ob_vel_t = cpvmult(ob_vel, dt)
+                    ob_av = cpvadd(ob_pos, ob_vel_t)
+                    avoid_vec = cpvadd(ob_av, avoid_vec)
+                try:
+                    target = cpv(tup_targ[0], tup_targ[1])
+                    avoid_norm = cpvnormalize(avoid_vec)
+                    print avoid_norm
+                    print avoid_max
+                    new_av = cpvmult(avoid_norm, avoid_max)
+                    print new_av
+                    new_targ = cpvsub(target, new_av)
+                    tup_targ = new_targ.x, new_targ.y
+                except:
+                    pass
+            print tup_targ
+            steering_data._target = tup_targ
+
+
+    def execute_decision(self, SteeringComponent steering_data, 
+        SteeringAIComponent ai_data, str state):
+        if state == 'Wander':
+            self.choose_random_position(ai_data)
+
+    def choose_random_position(self, SteeringAIComponent ai_data):
+        cdef object gameworld = self.gameworld
+        map_size = gameworld.currentmap.map_size
+        cdef tuple new_position =  (randint(0, map_size[0]), 
+            randint(0, map_size[1]))
+        ai_data._target = new_position
 
 class SteeringSystem(GameSystem):
     physics_system = StringProperty(None)
--- kivent_tutorials/5_controlling_physics_object/main.py
+++ kivent_tutorials/5_controlling_physics_object/main.py
@@ -12,6 +12,30 @@ from kivy.properties import NumericProperty, ListProperty
 from kivy.vector import Vector
 
 
+class ShipSystem(GameSystem):
+
+    def collide_with_view(self, space, arbiter):
+        gameworld = self.gameworld
+        entities = gameworld.entities
+        ent1_id = arbiter.shapes[0].body.data
+        ent2_id = arbiter.shapes[1].body.data
+        ent1 = entities[ent1_id]
+        ship_data = ent1.ship
+        in_view = ship_data.in_view
+        in_view.add(ent2_id)
+        return False
+
+    def separate_from_view(self, space, arbiter):
+        gameworld = self.gameworld
+        entities = gameworld.entities
+        ent1_id = arbiter.shapes[0].body.data
+        ent2_id = arbiter.shapes[1].body.data
+        ent1 = entities[ent1_id]
+        ship_data = ent1.ship
+        in_view = ship_data.in_view
+        in_view.discard(ent2_id)
+        return False
+
 
 class TestGame(Widget):
     current_entity = NumericProperty(None)
@@ -32,8 +56,8 @@ class TestGame(Widget):
         gameworld = self.gameworld
         entities = gameworld.entities
         entity = entities[self.current_entity]
-        steering = entity.steering
-        steering.target = (touch.x, touch.y)
+        ai_data = entity.steering_ai
+        ai_data.target = (touch.x, touch.y)
 
     def draw_some_stuff(self):
         size = Window.size
@@ -41,6 +65,10 @@ class TestGame(Widget):
             pos = (250, 250)
             ship_id = self.create_ship(pos)
             self.current_entity = ship_id
+        for x in range(10):
+            pos = randint(0, size[0]), randint(0, size[1])
+            self.create_asteroid(pos)
+
 
     def no_collide(self, space, arbiter):
         return False
@@ -48,20 +76,55 @@ class TestGame(Widget):
     def setup_collision_callbacks(self):
         systems = self.gameworld.systems
         physics_system = systems['physics']
+        ship_system = systems['ship']
         physics_system.add_collision_handler(
             1, 2, 
             begin_func=self.no_collide)
+        physics_system.add_collision_handler(
+            2, 3, 
+            begin_func=ship_system.collide_with_view,
+            separate_func=ship_system.separate_from_view)
+
+    def create_asteroid(self, pos):
+        x_vel = randint(0, 10)
+        y_vel = randint(0, 10)
+        angle = radians(randint(-360, 360))
+        angular_velocity = radians(randint(-150, -150))
+        shape_dict = {'inner_radius': 0, 'outer_radius': 32, 
+            'mass': 50, 'offset': (0, 0)}
+        col_shape = {'shape_type': 'circle', 'elasticity': .5, 
+            'collision_type': 3, 'shape_info': shape_dict, 'friction': 1.0}
+        col_shapes = [col_shape]
+        physics_component = {'main_shape': 'circle', 
+            'velocity': (x_vel, y_vel), 
+            'position': pos, 'angle': angle, 
+            'angular_velocity': angular_velocity, 
+            'vel_limit': 250, 
+            'ang_vel_limit': radians(200), 
+            'mass': 50, 'col_shapes': col_shapes}
+        create_component_dict = {'physics': physics_component, 
+            'physics_renderer': {'texture': 'asteroid1', 'size': (64 , 64)}, 
+            'position': pos, 'rotate': 0}
+        component_order = ['position', 'rotate', 
+            'physics', 'physics_renderer']
+        return self.gameworld.init_entity(create_component_dict, component_order)
 
     def create_ship(self, pos):
         x_vel = 0
         y_vel = 0
         angle = 0
         angular_velocity = 0
+        view_distance = 200
+        view_dict = {'vertices': [(0., 0.), (0, 88.), 
+            (view_distance, 108.), (view_distance, -20.)],
+            'offset': (96,44.)}
+        view_shape_dict = {'shape_type': 'poly', 'elasticity': 0.0, 
+            'collision_type':2, 'shape_info': view_dict, 'friction': 0.0}
         shape_dict = {'inner_radius': 0, 'outer_radius': 45, 
             'mass': 10, 'offset': (0, 0)}
         col_shape = {'shape_type': 'circle', 'elasticity': .0, 
             'collision_type': 1, 'shape_info': shape_dict, 'friction': .7}
-        col_shapes = [col_shape]
+        col_shapes = [col_shape, view_shape_dict]
         physics_component = {'main_shape': 'circle', 
             'velocity': (x_vel, y_vel), 
             'position': pos, 'angle': angle, 
@@ -70,16 +133,24 @@ class TestGame(Widget):
             'ang_vel_limit': radians(900), 
             'mass': 50, 'col_shapes': col_shapes}
         steering_component = {
-            'turn_speed': 10.0,
-            'stability': 900000.0,
-            'max_force': 200000.0,
-            'speed': 350,
+            'turn_speed': 4.0,
+            'stability': 600000.0,
+            'max_force': 100000.0,
+            'speed': 200.,
             }
+        steering_ai_component = {
+            'avoidance_max': 150.,
+            'change_time': 2.0,
+            'state': 'Wander'
+            }
+        ship_component = {'in_view': set()}
         create_component_dict = {'physics': physics_component, 
             'physics_renderer': {'texture': 'ship7', 'size': (96 , 88)}, 
-            'position': pos, 'rotate': 0, 'steering': steering_component}
+            'position': pos, 'rotate': 0, 'steering': steering_component,
+            'steering_ai': steering_ai_component, 'ship': ship_component}
         component_order = ['position', 'rotate', 
-            'physics', 'physics_renderer', 'steering']
+            'physics', 'physics_renderer', 'steering', 'steering_ai',
+            'ship']
         return self.gameworld.init_entity(create_component_dict, component_order)
 
     def setup_map(self):
